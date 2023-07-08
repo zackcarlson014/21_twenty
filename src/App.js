1d4b8ba4e162010c3c9e1070928faf20337a86db
zackcarlson014 <jzcarlson014@gmail.com>
2023-07-08 15:41:22 -0500

    template formatting - linting

--- src/App.js
+++ src/App.js
@@ -71,6 +71,7 @@ const App = ({ signOut }) => {
   return (
     <View className="App">
       <Heading level={1}>My Notes App</Heading>
+      
       <View as="form" margin="3rem 0" onSubmit={createNote}>
         <Flex direction="row" justifyContent="center">
           <TextField
@@ -81,6 +82,7 @@ const App = ({ signOut }) => {
             variation="quiet"
             required
           />
+
           <TextField
             name="description"
             placeholder="Note Description"
@@ -89,18 +91,22 @@ const App = ({ signOut }) => {
             variation="quiet"
             required
           />
+
           <View
             name="image"
             as="input"
             type="file"
             style={{ alignSelf: "end" }}
           />
+
           <Button type="submit" variation="primary">
             Create Note
           </Button>
         </Flex>
       </View>
+
       <Heading level={2}>Current Notes</Heading>
+
       <View margin="3rem 0">
         {notes.map((note) => (
           <Flex
@@ -112,7 +118,11 @@ const App = ({ signOut }) => {
             <Text as="strong" fontWeight={700}>
               {note.name}
             </Text>
-            <Text as="span">{note.description}</Text>
+
+            <Text as="span">
+              {note.description}
+            </Text>
+
             {note.image && (
               <Image
                 src={note.image}
@@ -120,12 +130,14 @@ const App = ({ signOut }) => {
                 style={{ width: 400 }}
               />
             )}
+
             <Button variation="link" onClick={() => deleteNote(note)}>
               Delete note
             </Button>
           </Flex>
         ))}
       </View>
+  
       <Button onClick={signOut}>Sign Out</Button>
     </View>
   );
